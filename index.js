c1ead14098f99282c98bc3022923c1763e778cae
Joy Christine <joychristin2@gmail.com>
2020-09-17 11:14:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// alert("welcome To my library");
+let myLibrary = [];
+
+//constructor function
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+let addbtn = document.querySelector(".mybtn");
+let modal = document.querySelector(".modal");
+let closeButton = document.querySelector(".form-close");
+
+addbtn.addEventListener("click", showModal);
+closeButton.addEventListener("click", hideModal);
+
+function showModal() {
+    modal.style.display = "block";
+}
+
+function hideModal() {
+    modal.style.display = "none";
+}
